c1dbef3a4cf8be338bd6853a9854d675c0fe6a11
Roy Canseco <roy.canseco@gmail.com>
2025-01-19 12:44:58 +0800

    add number filtering question

---
+++ public/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.createElement("nav");
+    nav.innerHTML = `
+        <a href="index.html">Home</a>
+        <a href="q-silver_anniv.html">Silver Anniversary Lesson</a>
+        <a href="q-counting_maybes.html">Counting Maybes Lesson</a>
+        <a href="q-paragraph_text.html">Get Paragraph Content Lesson</a>
+        <a href="q-object_properties.html">Object Properties Lesson</a>
+        <a href="q-number_filter.html">Number Filter Lesson</a>
+    `;
+    document.body.insertBefore(nav, document.body.firstChild);
+});
